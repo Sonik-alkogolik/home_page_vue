0878a5cfd157a1d628c194ec99005c4b704aeebe
Дмитрий Белицкий <belickij@jam-it.ru>
2023-06-01 10:33:20 +0300

    del logo

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <nav class="navbar">
         <div class="container">
           <div class="navbar-content">
-            <div class="logo">VUE-CLI</div>
             <ul class="navbar-list">
               <li class="navbar-item" v-for="link in links" :key="link.title">
                 <router-link
